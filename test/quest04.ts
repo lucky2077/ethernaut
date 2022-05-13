4734c6d9e95d2906c0381be8b785f583ef667ee0
Lucky <lucky2077@mailverse.xyz>
2022-05-13 11:53:03 +0800

    finish quest04

---
+++ test/quest04.ts
@@ -0,0 +1,27 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Quest03", function () {
+  it("Should takeover Telephone contract", async function () {
+    const [owner, user1] = await ethers.getSigners();
+
+    const Telephone = await ethers.getContractFactory("Telephone");
+    const telephone = await Telephone.deploy();
+    await telephone.deployed();
+
+    console.log("telephone.address:", telephone.address);
+
+    expect(await telephone.owner()).to.equal(owner.address);
+
+    const Quest = await ethers.getContractFactory("Quest04");
+    const quest = await Quest.deploy();
+    await quest.deployed();
+
+    console.log("quest04.address:", quest.address);
+
+    const takeoverTX = await quest.takeover(telephone.address, user1.address);
+    await takeoverTX.wait();
+
+    expect(await telephone.owner()).to.equal(user1.address);
+  });
+});
